b97aba069c3c2ad0af6bd0d4e5e99daad15f9013
emmanouilvslk <emmanouilvslk@gmail.com>
2021-10-17 12:17:34 +0300

    user profile picture, pagination-filtering-sort data

--- src/models/user.js
+++ src/models/user.js
@@ -4,52 +4,56 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Task = require("../models/task");
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error("Email is invalid");
-            }
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
         },
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes("password")) {
-                throw new Error("Password can't contain the word password");
-            }
-        },
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error("Age must be a positive number");
-            }
-        },
-    },
-    tokens: [
-        {
-            token: {
-                type: String,
-                required: true,
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error("Email is invalid");
+                }
             },
         },
-    ],
-});
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 7,
+            validate(value) {
+                if (value.toLowerCase().includes("password")) {
+                    throw new Error("Password can't contain the word password");
+                }
+            },
+        },
+        age: {
+            type: Number,
+            default: 0,
+            validate(value) {
+                if (value < 0) {
+                    throw new Error("Age must be a positive number");
+                }
+            },
+        },
+        tokens: [
+            {
+                token: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+        avatar: { type: Buffer },
+    },
+    { timestamps: true }
+);
 
 userSchema.virtual("tasks", {
     ref: "Task",
@@ -62,6 +66,7 @@ userSchema.methods.toJSON = function () {
     const userObject = user.toObject();
     delete userObject.password;
     delete userObject.tokens;
+    delete userObject.avatar;
 
     return userObject;
 };
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,8 @@ const router = new express.Router();
 const User = require("../models/user");
 require("../db/mongoose");
 const auth = require("../middleware/auth");
+const multer = require("multer");
+const sharp = require("sharp");
 
 router.post("/users", async (req, res) => {
     const user = new User(req.body);
@@ -49,8 +51,54 @@ router.post("/users/logoutAll", auth, async (req, res) => {
     }
 });
 
+const upload = multer({
+    limits: {
+        fileSize: 1000000,
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error("Please upload a picture !"));
+        }
+        cb(undefined, true);
+    },
+});
+
+router.post(
+    "/users/me/avatar",
+    auth,
+    upload.single("avatar"),
+    async (req, res) => {
+        const buffer = await sharp(req.file.buffer)
+            .resize({ width: 250, height: 250 })
+            .png()
+            .toBuffer();
+        req.user.avatar = buffer;
+        await req.user.save();
+        res.send();
+    },
+    (error, req, res, next) => {
+        //we must provide these args in order express to understand that this function is provided to handle errors
+        res.status(400).send({ error: error.message });
+    }
+);
+
 router.get("/users/me", auth, async (req, res) => {
     res.send(req.user);
+});
+
+router.get("/users/:id/avatar", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user || !user.avatar) {
+            throw new Error("No user or avatar found!");
+        }
+
+        res.set("Content-Type", "image/png");
+        res.send(user.avatar);
+    } catch (e) {
+        res.status(400).send({ error: e });
+    }
 });
 
 router.patch("/users/me", auth, async (req, res) => {
@@ -89,6 +137,16 @@ router.delete("/users/me", auth, async (req, res) => {
         res.send(req.user);
     } catch (e) {
         res.status(500).send();
+    }
+});
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+    try {
+        req.user.avatar = undefined;
+        await req.user.save();
+        res.send(req.user);
+    } catch (e) {
+        res.status(500).send({ error: e.message });
     }
 });
 
